bd38db384cdfb872c7d3511801eb53321c1bcb0b
archanasreekumar <42196480+archanasreekumar@users.noreply.github.com>
2025-06-12 15:13:45 +0530

    Add files via upload

---
+++ Roman_to_Integer.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/roman-to-integer/ 
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        out=0
+        s=s.replace("IV","IIII").replace("IX","VIIII").replace("XL","XXXX").replace("XC","LXXXX").replace("CD","CCCC").replace("CM","DCCCC")
+        for i in s:
+            out+=map[i] 
+        return out
