7ab8d3d38b648a0cb4d14edb3aa3a0e5670c34a2
archanasreekumar <42196480+archanasreekumar@users.noreply.github.com>
2025-08-19 19:23:09 +0530

    Add files via upload

--- SLIDING_WINDW_best_time_buy_sell_stock_OPT.py
+++ SLIDING_WINDW_best_time_buy_sell_stock_OPT.py
@@ -8,19 +8,31 @@
 # Input: prices = [7,6,4,3,1]
 # Output: 0
 # Explanation: In this case, no transactions are done and the max profit = 0.
+#
+# Approach
+# For prices = [7, 1, 5, 3, 6, 4]:
+# Initialization: profit = 0, day = 7
+# Iteration:
+# i = 1: diff = 1 - 7 = -6, profit = max(-6, 0) = 0, day = 1 (since diff <= 0)
+# i = 2: diff = 5 - 1 = 4, profit = max(4, 0) = 4
+# i = 3: diff = 3 - 1 = 2, profit = max(2, 4) = 4
+# i = 4: diff = 6 - 1 = 5, profit = max(5, 4) = 5
+# i = 5: diff = 4 - 1 = 3, profit = max(3, 5) = 5
+# Result: 5
 
 
 def get_max_profit(prices):
     n = len(prices)
     max_profit = 0
+    buy_day_price = prices[0]
 
-    for i in range(n):
-        for j in range(i + 1, n):
-            profit = prices[j] - prices[i]
-            max_profit = max(max_profit, profit)
+    for i in range(1, n):
+        profit = prices[i] - buy_day_price
+        max_profit = max(max_profit, profit)
+        buy_day_price = min(buy_day_price, prices[i])
+
     return max_profit
 
 
 prices = [7, 1, 5, 3, 6, 4]
-# prices = [7,6,4,3,1]
 print(get_max_profit(prices))
