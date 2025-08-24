ab385a937c0249fff8f260ad76040cf75de28263
archanasreekumar <42196480+archanasreekumar@users.noreply.github.com>
2025-08-25 00:09:04 +0530

    Update Anagram_hasmap_get.py

--- Anagram_hasmap_get.py
+++ Anagram_hasmap_get.py
@@ -17,3 +17,16 @@ def is_anagram(word1, word2):
 word1 = "arrrchanaa"
 word2 = "rrachana"
 print(is_anagram(word1, word2))
+
+############ANOTHER EASY SOLUTION############################
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+
+#         countS, countT = {}, {}
+
+#         for i in range(len(s)):
+#             countS[s[i]] = 1 + countS.get(s[i], 0)
+#             countT[t[i]] = 1 + countT.get(t[i], 0)
+#         return countS == countT
